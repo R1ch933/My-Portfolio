f2a2bf45ec214b869849baf861faebd25c6840ec
rich <richardher32@gmail.com>
2020-08-10 11:39:38 -0500

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Background from './images/Background.jpg'
-import {Facebook, GitHub, Linkedin, CornerUpRight, CornerRightUp, ArrowRight, ArrowLeft} from 'react-feather'
+import {Facebook, GitHub, Linkedin, CornerUpRight, CornerRightUp, ArrowRight, ArrowLeft, X} from 'react-feather'
 import profile from './images/Profile.jpg'
 import aboutMePhoto from './images/AboutMe_Photo.jpg'
 import pdf from './images/Resume.pdf'
@@ -30,17 +30,20 @@ function App() {
     <div className="App">
       <NavBar s2={{section2}}/>
       <Header />
+      <div className="infoBackground">
       <About/>
       <Projects setPosition={setPositionProj1} setPosition2={setPositionProj2} setPosition3={setPositionProj3}/>
       <div className="projectBox" style={{left: pos.pos}}>
         <div className="xButton" onClick={retractPosition}>
-
+          <X height="10vh" width="10vh" color="white"/>
         </div>
       
       </div>
       <ProjectOne pos={pos}/>
       <ProjectTwo pos={pos}/>
       <ProjectThree pos={pos}/>
+      <Contact />
+      </div>
     </div>
   );
 }
@@ -48,7 +51,7 @@ function App() {
 const NavBar = ({s2}) => 
   <div className="navigationBar">
     <div className="Home">
-      <a href="#section1">Home</a>
+      <a href="">Home</a>
     </div>
     <div className="About">
       <a href="#section2">About</a>
@@ -57,10 +60,10 @@ const NavBar = ({s2}) =>
       <a href="#section3">Resume</a>
     </div>
     <div className="Projects">
-      <a href="">Projects</a>
+      <a href="#section4">Projects</a>
     </div>
     <div className="Contact">
-      <a href="">Contact</a>
+      <a href="#section5">Contact</a>
     </div>
   </div>
 
@@ -125,7 +128,8 @@ const About = () =>
   </div>
 
 const Projects = ({setPosition,setPosition2,setPosition3}) => 
-  <div className="projects">
+  <div className="projects" id="section4">
+    <hr style={{color:"black"}}/>
     <div>
         <h1>Projects</h1>
     </div>
@@ -143,7 +147,7 @@ const Projects = ({setPosition,setPosition2,setPosition3}) =>
 const ProjectOne = ({pos}) =>
   <div className="showProj1" style={{left: pos.posproj1}} >
   <div style={{position:"relative", height:"5vh", color:"white"}}>
-    <h3 style={{fontSize: "3vh"}}>Project 1: Reference-Dojo (Django)</h3>
+    <h3 style={{fontSize: "3vh"}}>Project 1: Reference-Dojo Web Application (Django)</h3>
   </div>
   <div className="imageBox">
     <img style={{width: "100%",height:"auto"}} src={project1}/>
@@ -162,17 +166,16 @@ const ProjectOne = ({pos}) =>
 const ProjectTwo = ({pos}) =>
   <div className="showProj1" style={{left: pos.posproj2}} >
   <div style={{position:"relative", height:"5vh", color:"white"}}>
-    <h3 style={{fontSize: "3vh"}}>Project 2: Don't Let It Rain</h3>
+    <h3 style={{fontSize: "3vh"}}>Project 2: Don't Let It Rain Game (React)</h3>
   </div>
   <div className="imageBox">
     <img style={{width: "100%",height:"auto"}} src={project2}/>
   </div>
   <div className="textBox" style={{textAlign:"left"}}>
-    <p style={{fontSize:"3vmin", marginLeft:"5px"}}>A web application created using the Django framework. It is deployed as an application on Heroku
-      with its domain name being <a target="_blank" href="http://reference-dojo.herokuapp.com/">reference-dojo.herokuapp.com</a>. Users can create accounts to upload
-      a word document file and store them on this website. Selecting uploaded documents will allow
-      users to see a list of MLA style citations used in the file itself. The files can then be downloaded or 
-      deleted off of the database. 
+    <p style={{fontSize:"3vmin", marginLeft:"5px"}}>A small game created using the React framework and deployed using firebase. It is heavily based off of the hangman game and can be
+      reached at <a href="https://don-t-let-it-rain.firebaseapp.com/" target="_blank">don-t-let-it-rain.firebaseapp.com</a>. It has a simplistic and interactive UI. Players can use 
+      hint coins to reveal characters of the word. This project was done to experiment more with animations and the
+      backend of React, the latter being productively more challenging than I had assumed.
     </p>
   </div>
 
@@ -181,20 +184,28 @@ const ProjectTwo = ({pos}) =>
 const ProjectThree = ({pos}) =>
 <div className="showProj1" style={{left: pos.posproj3}} >
 <div style={{position:"relative", height:"5vh", color:"white"}}>
-  <h3 style={{fontSize: "3vh"}}></h3>
+  <h3 style={{fontSize: "3vh"}}>Project 3: Master Password Android App (Kivy)</h3>
 </div>
 <div className="imageBox">
-  <ReactPlayer url="https://www.youtube.com/watch?v=od-e1Z3sPCE" />
+  <ReactPlayer url="https://youtu.be/WByDfL1Ayts" width="45vw" height="50vh"/>
 </div>
 <div className="textBox" style={{textAlign:"left"}}>
-  <p style={{fontSize:"3vmin", marginLeft:"5px"}}>A small game created using the React framework and deployed using firebase. It is heavily based off of the hangman game and can be
-      reached at <a href="https://don-t-let-it-rain.firebaseapp.com/" target="_blank">don-t-let-it-rain.firebaseapp.com</a>. It has a simplistic and interactive UI. Players can use 
-      hint coins to reveal characters of the word. This project was done to experiment more with animations and the
-      backend of React, the latter being productively more challenging than I had assumed.  
+  <p style={{fontSize:"3vmin", marginLeft:"5px"}}>An android app developed from the Kivy framework and compiled with buildozer. Outside of built-in
+  assets, the database and functionality was built compeltely from scratch which made this project the hardest to complete.
+  It is fully functional and can create accounts to store passwords inside of a catelog that can be deleted at any time.
+  Play the video for a demonstrations. 
   </p>
 </div>
 
 </div> 
+
+const Contact = () =>
+  <div className="contactBox" id="section5">
+    <hr></hr>
+    <h2>Contact</h2>
+    <h3>richardher32@gmail.com</h3>
+    <h3>913-749-8200</h3>
+  </div>
 
 let resizeTimer;
 window.addEventListener("resize", () => {
